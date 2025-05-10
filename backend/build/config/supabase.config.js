898f37351be6f4644044350e323332733e03e53c
Anubis1960 <alexander.rk19@gmail.com>
2025-05-11 01:52:31 +0300

    delete build

--- backend/build/config/supabase.config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const supabase_js_1 = require("@supabase/supabase-js");
-const secrets_utils_1 = require("../utils/secrets.utils");
-const supabase = (0, supabase_js_1.createClient)(secrets_utils_1.SUPABASE_URL, secrets_utils_1.SUPABASE_KEY);
-exports.default = supabase;
